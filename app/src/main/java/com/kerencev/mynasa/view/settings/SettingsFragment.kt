67bdf0f5bc36209a3a7967391cea1d6073ef2bd5
Sergei Kerencev <Kerentzev@bk.ru>
2022-07-02 16:37:09 +0300

    Поработал немного над цветами в темах

--- app/src/main/java/com/kerencev/mynasa/view/settings/SettingsFragment.kt
+++ app/src/main/java/com/kerencev/mynasa/view/settings/SettingsFragment.kt
@@ -1,8 +1,6 @@
 package com.kerencev.mynasa.view.settings
 
-import android.content.Context
 import android.os.Bundle
-import android.view.ContextThemeWrapper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
