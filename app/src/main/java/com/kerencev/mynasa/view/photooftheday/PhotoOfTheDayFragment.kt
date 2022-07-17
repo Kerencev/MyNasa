ed2ffc0dde538af4339521211f052f912680742d
Sergei Kerencev <Kerentzev@bk.ru>
2022-07-17 12:09:21 +0300

    Добавил Радугу в виде матрицы

--- app/src/main/java/com/kerencev/mynasa/view/photooftheday/PhotoOfTheDayFragment.kt
+++ app/src/main/java/com/kerencev/mynasa/view/photooftheday/PhotoOfTheDayFragment.kt
@@ -3,12 +3,16 @@ package com.kerencev.mynasa.view.photooftheday
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.text.Spannable
 import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import android.text.style.ImageSpan
 import android.view.*
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.toBitmap
+import androidx.core.text.toSpannable
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.transition.ChangeBounds
@@ -31,6 +35,7 @@ class PhotoOfTheDayFragment(private val viewPagerHandler: ViewPagerHandler) : Fr
     private var _binding: FragmentPhotoOfTheDayBinding? = null
     private val binding get() = _binding!!
     private var date: String? = null
+    lateinit var spannableRainbow: SpannableString
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -82,10 +87,59 @@ class PhotoOfTheDayFragment(private val viewPagerHandler: ViewPagerHandler) : Fr
                 placeholder(R.drawable.nasa)
             }
             tvPhotoDay.text = addImageSpan(pictureOfTheDayResponseData.explanation)
+            spannableRainbow = SpannableString(tvPhotoDay.text.toSpannable())
+            rainbow(1)
+
             imgPhotoDay.setOnClickListener {
                 viewPagerHandler.onImageClick(pictureOfTheDayResponseData.hdurl)
             }
         }
+
+    fun rainbow(i: Int = 1) {
+        var currentCount = i
+        val x = object : CountDownTimer(20000, 200) {
+            override fun onTick(millisUntilFinished: Long) {
+                colorText(currentCount)
+                currentCount = if (++currentCount > 5) 1 else currentCount
+            }
+
+            override fun onFinish() {
+                rainbow(currentCount)
+            }
+        }
+        x.start()
+    }
+
+    private fun colorText(colorFirstNumber: Int) {
+        binding.tvPhotoDay.setText(spannableRainbow, TextView.BufferType.SPANNABLE)
+        spannableRainbow = binding.tvPhotoDay.text as SpannableString
+        val map = mapOf(
+            0 to ContextCompat.getColor(requireContext(), R.color.green),
+            1 to ContextCompat.getColor(requireContext(), R.color.green1),
+            2 to ContextCompat.getColor(requireContext(), R.color.green2),
+            3 to ContextCompat.getColor(requireContext(), R.color.green3),
+            4 to ContextCompat.getColor(requireContext(), R.color.green4),
+            5 to ContextCompat.getColor(requireContext(), R.color.green4),
+            6 to ContextCompat.getColor(requireContext(), R.color.green4),
+        )
+        val spans = spannableRainbow.getSpans(
+            0, spannableRainbow.length,
+            ForegroundColorSpan::class.java
+        )
+        for (span in spans) {
+            spannableRainbow.removeSpan(span)
+        }
+
+        var colorNumber = colorFirstNumber
+        for (i in 0 until binding.tvPhotoDay.text.length) {
+            if (colorNumber == 5) colorNumber = 0 else colorNumber += 1
+            spannableRainbow.setSpan(
+                ForegroundColorSpan(map.getValue(colorNumber)),
+                i, i + 1,
+                Spannable.SPAN_EXCLUSIVE_INCLUSIVE
+            )
+        }
+    }
 
     private fun showSnackBarError() {
         binding.progressBar.visibility = View.GONE
